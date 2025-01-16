ae87955ef3f7d630690cc0c2954bf3bf1228c418
Slozzyondul <solomonondula@gmail.com>
2025-01-16 22:28:49 +0300

    update web 4

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -26,35 +26,104 @@ class _HomePageState extends State<HomePage> {
               );
             },
           ),
-          backgroundColor: Colors.brown,
+          // backgroundColor: Color.fromRGBO(248, 249, 250, 1),
+          backgroundColor: Color.fromRGBO(139, 94, 60, 1),
+
           title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              buildDropdownTab(context, "Dog Food", [
-                const PopupMenuItem(value: "dry", child: Text("Dry Food")),
-                const PopupMenuItem(value: "wet", child: Text("Wet Food")),
-                const PopupMenuItem(value: "snacks", child: Text("Snacks")),
-              ]),
-              buildDropdownTab(context, "Our Story", [
-                const PopupMenuItem(value: "about", child: Text("About Us")),
-                const PopupMenuItem(value: "values", child: Text("Our Values")),
-                const PopupMenuItem(value: "team", child: Text("Team")),
-              ]),
-              buildDropdownTab(context, "Contact Us", [
-                const PopupMenuItem(value: "email", child: Text("Email")),
-                const PopupMenuItem(value: "phone", child: Text("Phone")),
-                const PopupMenuItem(value: "location", child: Text("Location")),
-              ]),
+              const Spacer(),
+              buildDropdownTab(
+                context,
+                "Dog Food",
+                [
+                  const PopupMenuItem(
+                    value: "dry",
+                    child: Text(
+                      "Dry Food",
+                      style: TextStyle(color: Color.fromRGBO(139, 94, 60, 1)),
+                    ),
+                  ),
+                  const PopupMenuItem(
+                    value: "wet",
+                    child: Text(
+                      "Wet Food",
+                      style: TextStyle(color: Color.fromRGBO(139, 94, 60, 1)),
+                    ),
+                  ),
+                  const PopupMenuItem(
+                    value: "snacks",
+                    child: Text(
+                      "Snacks",
+                      style: TextStyle(color: Color.fromRGBO(139, 94, 60, 1)),
+                    ),
+                  ),
+                ],
+              ),
+              buildDropdownTab(
+                context,
+                "Our Story",
+                [
+                  const PopupMenuItem(
+                    value: "about",
+                    child: Text(
+                      "About Us",
+                      style: TextStyle(color: Color.fromRGBO(139, 94, 60, 1)),
+                    ),
+                  ),
+                  const PopupMenuItem(
+                    value: "values",
+                    child: Text(
+                      "Our Values",
+                      style: TextStyle(color: Color.fromRGBO(139, 94, 60, 1)),
+                    ),
+                  ),
+                  const PopupMenuItem(
+                    value: "team",
+                    child: Text(
+                      "Team",
+                      style: TextStyle(color: Color.fromRGBO(139, 94, 60, 1)),
+                    ),
+                  ),
+                ],
+              ),
+              buildDropdownTab(
+                context,
+                "Contact Us",
+                [
+                  const PopupMenuItem(
+                    value: "email",
+                    child: Text(
+                      "Email",
+                      style: TextStyle(color: Color.fromRGBO(139, 94, 60, 1)),
+                    ),
+                  ),
+                  const PopupMenuItem(
+                    value: "phone",
+                    child: Text(
+                      "Phone",
+                      style: TextStyle(color: Color.fromRGBO(139, 94, 60, 1)),
+                    ),
+                  ),
+                  const PopupMenuItem(
+                    value: "location",
+                    child: Text(
+                      "Location",
+                      style: TextStyle(color: Color.fromRGBO(139, 94, 60, 1)),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            
-          ],
+          children: [],
         ),
       ),
     );
   }
+
+  
 }
