0098f50e324d4675540d20a01e52ec6e6e8c17fa
solo <solomonondula@gmail.com>
2025-01-25 12:26:17 +0300

    web index update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "c827bdf754732da2b7c9c06067f3e817",
+"flutter_bootstrap.js": "086c273681ce5f230a496d3e141c47fa",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
@@ -42,7 +42,7 @@ const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "fb5614311a0fe955c7ab9ea544692d38",
 "index.html": "1d20d935627256852d802a7d06ca2793",
 "/": "1d20d935627256852d802a7d06ca2793",
-"main.dart.js": "b801771c485c7a6d9aa2df7a733c5ec1",
+"main.dart.js": "7f70d3d9f8767ede4a3361866d9b9ef6",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea"};
 // The application shell files that are downloaded before a service worker can
 // start.
