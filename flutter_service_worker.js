79c6adb48340513e1667b83f0cdf24577cabb9a2
solo <solomonondula@gmail.com>
2025-01-27 13:52:42 +0300

    update the website

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "086c273681ce5f230a496d3e141c47fa",
+"flutter_bootstrap.js": "c1f1a024851b477eb1ee3c2bfbe86c90",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
@@ -24,7 +24,7 @@ const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/AssetManifest.json": "b662cdf453980aabf6708970c6351ae7",
 "assets/FontManifest.json": "f9cc474e6ecf3aee2525a6f492866488",
 "assets/AssetManifest.bin": "eb8152f62165fbe54509817341dcefc8",
-"assets/fonts/MaterialIcons-Regular.otf": "79c3c6e48fafec24554c7405c164e531",
+"assets/fonts/MaterialIcons-Regular.otf": "5ae62fa541df92bcadc2a53aa13e98e1",
 "assets/AssetManifest.bin.json": "9ddea44e2d427dcdf3989b2efe0e0163",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/NOTICES": "d7e0ee14fb89db6da8d1f98faeb9686f",
@@ -42,7 +42,7 @@ const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "fb5614311a0fe955c7ab9ea544692d38",
 "index.html": "1d20d935627256852d802a7d06ca2793",
 "/": "1d20d935627256852d802a7d06ca2793",
-"main.dart.js": "7f70d3d9f8767ede4a3361866d9b9ef6",
+"main.dart.js": "044b6550c7adb95d34b3ecc01bf967dc",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea"};
 // The application shell files that are downloaded before a service worker can
 // start.
