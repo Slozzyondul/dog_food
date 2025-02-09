fa3c667a44972b00751303b2b0baabb7ffbc3937
solo <solomonondula@gmail.com>
2025-02-09 17:45:49 +0300

    match theme from trello

--- lib/constants/themes.dart
+++ lib/constants/themes.dart
@@ -1,9 +1,20 @@
+// import 'package:flutter/material.dart';
+
+// final class DogFoodAppTheme {
+//   static const backgroundColor = Color.fromRGBO(248, 249, 250, 1);
+//   static const themeBrownColor = Color.fromRGBO(179, 128, 86, 1);
+//   static const menuBrownColor = Color.fromRGBO(139, 94, 60, 1);
+//   static const primaryButtonColor = Color.fromRGBO(0, 77, 64, 1);
+//   static const primaryButtonTextColor = Color.fromRGBO(255, 255, 255, 1);
+// }
+
 import 'package:flutter/material.dart';
 
 final class DogFoodAppTheme {
-  static const backgroundColor = Color.fromRGBO(248, 249, 250, 1);
-  static const themeBrownColor = Color.fromRGBO(179, 128, 86, 1);
-  static const menuBrownColor = Color.fromRGBO(139, 94, 60, 1);
-  static const primaryButtonColor = Color.fromRGBO(0, 77, 64, 1);
+  static const backgroundColor = Color.fromRGBO(230, 197, 173, 1); // E6C5AD
+  static const themeBrownColor = Color.fromRGBO(150, 100, 67, 1); // 966443
+  static const menuBrownColor = Color.fromRGBO(211, 172, 139, 1); // D3AC8B
+  static const primaryButtonColor = Color.fromRGBO(51, 66, 38, 1); // 334226
+  static const secondaryButtonColor = Color.fromRGBO(88, 135, 100, 1); // 588764
   static const primaryButtonTextColor = Color.fromRGBO(255, 255, 255, 1);
 }
