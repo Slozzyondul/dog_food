53151e412920ac2d07b6b3476643f3f53d54d1dc
solo <solomonondula@gmail.com>
2025-02-13 19:00:06 +0300

    price updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "a073f0cfb51f4fc1ba2abe46aefebe39",
+"flutter_bootstrap.js": "58020cd4167f7254d23ff273eb773450",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
@@ -27,7 +27,7 @@ const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/fonts/MaterialIcons-Regular.otf": "b8abce0785d4fab14ba4e25664108a37",
 "assets/AssetManifest.bin.json": "9ddea44e2d427dcdf3989b2efe0e0163",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
-"assets/NOTICES": "1aa20bf36b120e1bff47ccbb28b15565",
+"assets/NOTICES": "c6b6af6505b69e70da0700f98808a6db",
 "manifest.json": "840846dbeb387492450be5bba351aaba",
 "canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",
 "canvaskit/chromium/canvaskit.js.symbols": "e115ddcfad5f5b98a90e389433606502",
@@ -42,7 +42,7 @@ const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "fb5614311a0fe955c7ab9ea544692d38",
 "index.html": "1d20d935627256852d802a7d06ca2793",
 "/": "1d20d935627256852d802a7d06ca2793",
-"main.dart.js": "7e5c7195369d4a06c82897385b8f1a08",
+"main.dart.js": "4661e961c943d5c6b8b474ede0a98dd7",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea"};
 // The application shell files that are downloaded before a service worker can
 // start.
