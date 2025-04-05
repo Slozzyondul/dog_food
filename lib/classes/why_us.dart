3adb7810b6fc1dd01347df2a927d3bcccf06476f
Slozzyondul <solomonondula@gmail.com>
2025-04-05 16:04:09 +0300

    adjust text font in why us

--- lib/classes/why_us.dart
+++ lib/classes/why_us.dart
@@ -29,7 +29,7 @@ class _WhyUsState extends State<WhyUs> {
           verticalMargin8,
           Text(
             "Dogs are family. At Paws Kenya, we craft premium meals and treats to keep them happy and healthy. It's not just pet food - it's about the bond you share.",
-            style: Theme.of(context).textTheme.bodyLarge,
+            style: Theme.of(context).textTheme.bodyMedium,
             textAlign: TextAlign.center,
           ),
           verticalMargin16,
